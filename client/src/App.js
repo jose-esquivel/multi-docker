e29255f58443dc66645b7659a54710e873275395
José Esquivel <jesquivel@humanlongevity.com>
2019-10-02 11:55:30 -0500

    change app text

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Enter a number below to calculate the value of the Fibonacci sequence at that index.
           </p>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
